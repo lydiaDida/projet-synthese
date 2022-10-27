359dad72c0532e942241fe3a85c22528639f6aed
HaappyThomas <happy.thomas.wang@gmail.com>
2022-10-27 17:36:59 -0400

    add entity CarteCredit

---
+++ src/main/java/ca/bdeb/projetsynthese/models/CarteCredit.java
@@ -0,0 +1,56 @@
+package ca.bdeb.projetsynthese.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Date;
+
+/**
+ * Created by Thomas Wang on 10/27/2022.
+ */
+@Entity
+@Table(name = "CarteCredit")
+public class CarteCredit {
+    @Id
+    @Column(name = "numero")
+    private String numero;
+
+    @Column(name = "expiration")
+    private Date expiration;
+
+
+
+    // constructor
+    public CarteCredit() {
+    }
+
+    public CarteCredit(String numero, Date expiration) {
+        this.numero = numero;
+        this.expiration = expiration;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public Date getExpiration() {
+        return expiration;
+    }
+
+    public void setExpiration(Date expiration) {
+        this.expiration = expiration;
+    }
+
+    @Override
+    public String toString() {
+        return "CarteCredit{" +
+                "numero='" + numero + '\'' +
+                ", expiration=" + expiration +
+                '}';
+    }
+}
