36134ebb1d86b1602c1f098607b52b986d966225
HaappyThomas <happy.thomas.wang@gmail.com>
2022-11-08 09:05:02 -0500

    integer swagger3.0 dans le projet

--- src/main/java/ca/bdeb/projetsynthese/controller/AdministrateurController.java
+++ src/main/java/ca/bdeb/projetsynthese/controller/AdministrateurController.java
@@ -2,6 +2,8 @@ package ca.bdeb.projetsynthese.controller;
 
 import ca.bdeb.projetsynthese.dao.AdministrateurRepository;
 import ca.bdeb.projetsynthese.models.Administrateur;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import net.bytebuddy.asm.Advice;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,17 +15,24 @@ import java.util.List;
 /**
  * Created by Thomas Wang on 10/31/2022.
  */
+@Api(tags = "api pour administrateur")
 @RestController
+@RequestMapping("/api/v1/administrateur")
 public class AdministrateurController {
     @Autowired
     private AdministrateurRepository administrateurRepository;
 
+    @ApiOperation(value = "requête la list d'administrateur")
     @GetMapping("/admin")
     public List<Administrateur> findAllAdministrateurs(){
         return administrateurRepository.findAll();
     }
-//    @GetMapping("/test")
-//    public String  findTestAllAdministrateurs(){
-//        return "test for allAdministrate";
-//    }
+
+
+    // ce methode est pour test ce controller
+    @ApiOperation(value="test access administrateur controller")
+    @GetMapping("/test")
+    public String  findTestAllAdministrateurs(){
+        return "test for allAdministrate";
+    }
 }
