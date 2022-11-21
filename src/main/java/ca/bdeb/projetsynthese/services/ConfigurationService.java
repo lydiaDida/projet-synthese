6909a32d9ae47bc243c2467c589f66c47e28a9cf
HaappyThomas <happy.thomas.wang@gmail.com>
2022-11-21 09:23:06 -0500

    2022-11-21: modify ConfigurationService

--- src/main/java/ca/bdeb/projetsynthese/services/ConfigurationService.java
+++ src/main/java/ca/bdeb/projetsynthese/services/ConfigurationService.java
@@ -8,10 +8,5 @@ import java.lang.module.Configuration;
  * Created by Thomas Wang on 11/21/2022.
  */
 public class ConfigurationService {
-    //todo: Luisa tu ajouter une methonde pour avoir tous les champs de configuration
-    public Configuration getConfiguration() {
-        @Autowired
-                private Configuration  = repository.findAll();
-    }
-    //todo: Lydia ajouter une methode pour modifier les champs de configuration
+    // todo: implement
 }
